aa44de4e5360172b0197bdd404aa45e31593f4a7
Rinku sharma <rinkusharma.official@yahoo.com>
2024-03-13 16:12:33 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "myhotstar" # Replace with your actual S3 bucket name
+    bucket = "hotstar-clone2" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-southeast-2"
+    region = "us-east-2"
   }
 }
